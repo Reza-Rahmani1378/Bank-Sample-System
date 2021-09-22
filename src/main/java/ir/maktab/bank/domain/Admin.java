c7f899d1a6b7fb2f74ea41309830f2af6b353a6c
ASUS CENETR QOM <reza6273757@gmail.com>
2021-09-22 12:57:18 -0700

    add entity Admin

---
+++ src/main/java/ir/maktab/bank/domain/Admin.java
@@ -0,0 +1,24 @@
+package ir.maktab.bank.domain;
+
+import ir.maktab.bank.domain.base.User;
+import ir.maktab.bank.domain.enumeration.UserType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Admin extends User {
+
+
+    public Admin(String firstname, String lastname, String nationalCode,
+                 Date birthday, String username, String password, UserType usertype) {
+        super(firstname, lastname, nationalCode, birthday, username, password, usertype);
+    }
+
+}
