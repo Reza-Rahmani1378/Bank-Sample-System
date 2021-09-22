72bbd6d0114f9642e70d424ad1291fa68bd4a56d
ASUS CENETR QOM <reza6273757@gmail.com>
2021-09-22 12:55:29 -0700

    add Base

---
+++ src/main/java/ir/maktab/bank/base/repository/BaseRepository.java
@@ -0,0 +1,22 @@
+package ir.maktab.bank.base.repository;
+
+import ir.maktab.bank.base.domain.BaseEntity;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseRepository<E extends BaseEntity<ID>, ID extends Serializable> {
+
+    E save(E e);
+
+    void delete(E e);
+
+    E findById(ID id);
+
+    List<E> findAll();
+
+    boolean existById(ID id);
+
+    EntityManager getEntityManager();
+}
---
+++ src/main/java/ir/maktab/bank/base/service/BaseService.java
@@ -0,0 +1,20 @@
+package ir.maktab.bank.base.service;
+
+import ir.maktab.bank.base.domain.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<E extends BaseEntity<ID>
+        , ID extends Serializable> {
+
+    E save(E e);
+
+    void delete(E e);
+
+    E findById(ID id);
+
+    List<E> findAll();
+
+    boolean existById(ID id);
+}
