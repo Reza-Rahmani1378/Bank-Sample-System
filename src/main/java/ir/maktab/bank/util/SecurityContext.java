d1809d75dc2d8e59414452026eb690fd48db6982
ASUS CENETR QOM <reza6273757@gmail.com>
2021-09-22 13:00:33 -0700

    add SecurityContext

---
+++ src/main/java/ir/maktab/bank/util/SecurityContext.java
@@ -0,0 +1,24 @@
+package ir.maktab.bank.util;
+
+import ir.maktab.bank.domain.base.User;
+
+public class SecurityContext {
+
+    public SecurityContext() {
+    }
+
+    private static User activeUser;
+
+    public static void logOut() {
+        activeUser = null;
+    }
+
+    public static User getActiveUser() {
+        return activeUser;
+    }
+
+    public static void setActiveUser(User activeUser) {
+        SecurityContext.activeUser = activeUser;
+    }
+
+}
